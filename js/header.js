e8aff0093bcac7aff7de7e022d5095c3a9171b96
MaryoryChavez <maryory.chavez@gmail.com>
2017-04-18 05:36:10 -0500

    trabajo talentFest

---
+++ js/header.js
@@ -0,0 +1,28 @@
+function myFunction() {
+    var x = document.getElementById("navegacionPrincipal");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+    
+    videoModal();
+}
+
+function videoModal(){
+    /* Get iframe src attribute value i.e. YouTube video url
+    and store it in a variable */
+    var url = $("#videoPublicitario").attr('src');
+    
+    /* Assign empty url value to the iframe src attribute when
+    modal hide, which stop the video playing */
+    $("#modalVideo").on('hide.bs.modal', function(){
+        $("#videoPublicitario").attr('src', '');
+    });
+    
+    /* Assign the initially stored url back to the iframe src
+    attribute when modal is displayed again */
+    $("#modalVideo").on('show.bs.modal', function(){
+        $("#videoPublicitario").attr('src', url);
+    });
+}
